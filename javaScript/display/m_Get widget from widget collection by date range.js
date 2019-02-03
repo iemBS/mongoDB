8d5ac50682279752c140c1720b5a76b48fe899dc
sburnell <sburnell@gmx.de>
2019-02-03 03:09:41 -0800

    Add files via upload

---
+++ javaScript/display/m_Get widget from widget collection by date range.js
@@ -0,0 +1,49 @@
+-- how to connect to MongoDB collections
+- Using http://MongoLab.com
+- cred: superlumins, [other]
+- MongoDB version 2.6 (will be 3.0 on 150930)
+- open command window
+- navigate to this location on my computer:
+	C:\Program Files\MongoDB\Server\3.0\bin\
+- Connection command to run in R console:
+	mongo ds031701.mongolab.com:31701/dance_by_me -u <dbuser> -p <dbpassword>
+
+var filterDate
+var filterDateStart = ISODate('2016-06-02T00:00:00.000Z')
+var filterDateEnd = ISODate('2016-07-20T00:00:00.000Z')
+
+var eventCursor = db.event.find(
+			{
+				'time.start':{
+					'$gte': filterDateStart,
+					'$lte': filterDateEnd
+				}
+			},
+			{
+				"time.start":1,
+				"venue.state":1,
+				"venue.city":1,
+				name:1,
+				type:1,
+				danceType:1,
+				url:1,
+				_id:0
+			}
+).sort({"time.start":1,"venue.state":1,"venue.city":1}).pretty();
+
+	//- Test how many documents are in cursor
+	//eventCursor.count()
+
+eventCursor.forEach(
+	function(x){
+		print("start: " + (x.time.start).toISOString().substring(0,16).replace("T"," @ "));
+		print("city: " + x.venue.city + ", " + x.venue.state);
+		print("name: " + x.name);
+		print("type: " + x.type);
+		print("dance type: " + x.danceType);
+		print("URL: " + x.url);
+		print("");
+	}
+);
+
+
